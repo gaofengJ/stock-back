40605e3bd1b5e321753ee89555ef1e06c780b1a7
木风 <>
2022-03-08 20:42:33 +0800

    [faat]新增axios

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,10 +1,14 @@
-import Axios, { AxiosRequestConfig } from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+import axiosRetry from 'axios-retry';
 
-const client = Axios.create({
+const client = axios.create({
   // 配置
 });
 
-export async function request(url: string, config?:AxiosRequestConfig) {
+// 当请求失败后，自动重新请求，只有3次失败后才真正失败
+axiosRetry(client, { retries: 3 });
+
+export async function request(url: string, config?: AxiosRequestConfig) {
   const response = await client.request({ url, ...config });
   const result = response.data;
   // 业务
---
+++ src/shell/tushare/daily.js
@@ -0,0 +1,8 @@
+// import { v4 } from 'uuid';
+import * as aaa from 'mufeng-tools';
+// import { daily } from '@/api/tushare/index';
+
+// const dateArgv = process.argv[2]; // node daily.js '2021-01-04'
+// let date = dateArgv ? new Date(dateArgv) : new Date(); // 如果命令行中没有加日期，就使用当天日期
+// date = dateFormat(date, 'yyyyMMdd');
+console.log(aaa.dateFormat);
