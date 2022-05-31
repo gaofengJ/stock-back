75585189902e291cd47ed964507927ae9e134b4f
木风 <>
2022-06-01 00:55:11 +0800

    [feat]修改定时任务

--- src/schedule/index.ts
+++ src/schedule/index.ts
@@ -1,14 +1,26 @@
 import schedule from 'node-schedule';
+import { dateFormat } from 'mufeng-tools';
+import CurdManualService from '@/services/curd/manual';
 
 // 定义规则
 const rule = new schedule.RecurrenceRule();
 
-rule.second = [0, 10, 20, 30, 40, 50]; // 每隔 10 秒执行一次
+rule.dayOfWeek = [1, 2, 3, 4, 5]; // 每周一到周五执行
+rule.hour = 17;
+rule.minute = 40;
+rule.second = 0;
 
+/**
+ * 导入每日数据
+ */
 const dailyImport = () => {
-  console.log(111);
-  schedule.scheduleJob(rule, () => {
-    console.log(new Date());
+  console.log('定时任务-导入每日数据启动');
+  schedule.scheduleJob(rule, async () => {
+    console.log(`定时任务-导入每日数据开始，导入日期：${dateFormat(new Date(), 'yyyyMMdd')}`);
+    // eslint-disable-next-line no-param-reassign
+    const date: string = dateFormat(new Date(), 'yyyyMMdd');
+    await CurdManualService.manualImport(date);
+    console.log(`定时任务-导入每日数据结束，导入日期：${dateFormat(new Date(), 'yyyyMMdd')}`);
   });
 };
 
